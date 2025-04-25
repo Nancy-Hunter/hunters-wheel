99d3bd9a94e4830f0f310ffdadecdc205226fc61
Nancy Hunter <hunternancym@gmail.com>
2025-04-24 22:03:46 -0400

    multer middleware updated to be letter case inclusive

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,11 +4,18 @@ const path = require("path");
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("File type is not supported"), false);
-      return;
+    const allowedExtensions = [".jpg", ".jpeg", ".png"];
+    const allowedMimeTypes = ["image/jpeg", "image/png", "image/pjpeg"];
+    let ext = path.extname(file.originalname).toLowerCase();
+    const mime = file.mimetype;
+
+    const isExtAllowed = allowedExtensions.includes(ext);
+    const isMimeAllowed = allowedMimeTypes.includes(mime);
+
+    if (!isExtAllowed || !isMimeAllowed) {
+      return cb(new Error("File type is not supported"), false);
     }
+
     cb(null, true);
   },
 });
